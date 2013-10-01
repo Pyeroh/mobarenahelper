e1e90b835d1a1a5a8cafd71a1f90a2d1c5e53b5e
Pyeroh <quentin.nanteuil@gmail.com>
2013-10-01 15:10:46 +1100

    MAJ EItem

--- MobArena_Helper/src/model/enums/EItem.java
+++ MobArena_Helper/src/model/enums/EItem.java
@@ -2,6 +2,31 @@ package model.enums;
 
 public enum EItem {
 	
+	arc(0,0,"bow",EItemCat.arme);
 	
+	private int id;
+	private int meta;
+	private String name;
+	private EItemCat category;
+	
+	private EItem(int id, int meta, String name, EItemCat category) {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public int getMeta() {
+		return meta;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public EItemCat getCategory() {
+		return category;
+	}
 	
 }
