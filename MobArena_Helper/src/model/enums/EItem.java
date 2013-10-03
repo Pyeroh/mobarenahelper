839a65ec780944ff082c9ee5727d8d9f96b87506
Pyeroh <quentin.nanteuil@gmail.com>
2013-10-03 12:03:44 +1100

    -

--- MobArena_Helper/src/model/enums/EItem.java
+++ MobArena_Helper/src/model/enums/EItem.java
@@ -4,7 +4,36 @@ import java.util.ArrayList;
 
 public enum EItem {
 	
-	arc(0,0,"bow",EItemCat.weapon);
+	stone(1,0,"stone",EItemCat.block),
+	grass(0,0,null,null),
+	dirt(0,0,null,null),
+	cobblestone(0,0,null,null),
+	oakplank(0,0,null,null),
+	redplank(0,0,null,null),
+	birchplank(0,0,null,null),
+	jungleplank(0,0,null,null),
+	oaksapling(0,0,null,null),
+	redsapling(0,0,null,null),
+	birchsapling(0,0,null,null),
+	junglesapling(0,0,null,null),
+	sand(0,0,null,null),
+	gravel(0,0,null,null),
+	goldore(0,0,null,null),
+	ironore(0,0,null,null),
+	coalore(0,0,null,null),
+	oakwood(0,0,null,null),
+	redwood(0,0,null,null),
+	birchwood(0,0,null,null),
+	junglewood(0,0,null,null),
+	oakleaf(0,0,null,null),
+	redleaf(0,0,null,null),
+	birchleaf(0,0,null,null),
+	jungleleaf(0,0,null,null),
+	glass(0,0,null,null),
+	lapisore(0,0,null,null),
+	lapisblock(0,0,null,null),
+	dull(0,0,null,null),
+	;
 	
 	private int id;
 	private int meta;
