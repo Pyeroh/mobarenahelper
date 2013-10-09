6c1c5973c0bfa0463497e319eb8bbe85f5e55a56
Pyeroh <quentin.nanteuil@gmail.com>
2013-10-09 13:09:40 +1100

    -

--- MobArena_Helper/src/view/Launch.java
+++ MobArena_Helper/src/view/Launch.java
@@ -1,9 +1,7 @@
 package view;
 
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
+import model.enums.EItem;
 
-import org.yaml.snakeyaml.Yaml;
 
 public class Launch {
 
@@ -11,14 +9,19 @@ public class Launch {
 		/*UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
 		new MenuPrincipal();*/
 		
-		Yaml yaml = new Yaml();
+		EItem[] values = EItem.values();
+		for(int i=0;i<values.length;i++){
+			System.out.println(values[i].getName()+" : "+values[i].getImage().toString());
+		}
+		
+		/*Yaml yaml = new Yaml();
 		ArrayList<String> test = new ArrayList<>();
 		test.add("test1");
 		test.add("test2");
 		test.add("test3");
 		LinkedHashMap<String, Object> map = new LinkedHashMap<String, Object>();
 		map.put("test", test);
-		System.out.println(yaml.dumpAsMap(map));
+		System.out.println(yaml.dumpAsMap(map));*/
 	}
 
 }
