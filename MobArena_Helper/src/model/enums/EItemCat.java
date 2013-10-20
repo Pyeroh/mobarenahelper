8343154cafb2e86cf23df9bb17e081be73b5043c
Pyeroh <quentin.nanteuil@gmail.com>
2013-10-20 11:37:27 +1100

    Name for gui display

--- MobArena_Helper/src/model/enums/EItemCat.java
+++ MobArena_Helper/src/model/enums/EItemCat.java
@@ -1,14 +1,25 @@
 package model.enums;
 
 public enum EItemCat {
-	block,
-	decoration,
-	redstone,
-	transport,
-	misc,
-	food,
-	tool,
-	weapon,
-	potion,
-	materials;
+	block("Building blocks"),
+	decoration("Decoration blocks"),
+	redstone("Redstone"),
+	transport("Transportation"),
+	misc("Miscellaneous"),
+	food("Foodstuffs"),
+	tool("Tools"),
+	weapon("Combat"),
+	potion("Brewing"),
+	materials("Materials");
+	
+	private String gui_name;
+	
+	EItemCat(String gui_name) {
+		this.gui_name = gui_name;
+	}
+
+	public String getGui_name() {
+		return gui_name;
+	}
+
 }
