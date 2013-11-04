d8bd4a066737594ace75adbf1603ea9060ba10a8
Pyeroh <quentin.nanteuil@gmail.com>
2013-11-05 10:24:22 +1100

    Creation of Enchanter dialog

---
+++ MobArena_Helper/src/view/dialogs/Enchanter.java
@@ -0,0 +1,17 @@
+package view.dialogs;
+
+import javax.swing.JDialog;
+
+import model.Item;
+
+public class Enchanter extends JDialog {
+
+	private static final long serialVersionUID = -6398387470645060005L;
+	private JDialog dial;
+	private Item item;
+	
+	public Enchanter(JDialog dial, Item item) {
+		//item.getEnchantements()
+	}
+
+}
